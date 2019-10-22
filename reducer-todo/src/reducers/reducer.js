cbd7ba0e4b0ea3c90d36421bb957f8c7b9f19769
Brandon Green <jailhousejordans@gmail.com>
2019-10-22 17:03:30 -0500

    finally

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,40 +1,52 @@
 
 
-
 export const initialState = [
     {
-        item: 'Learn about reducers',
-        completed: false,
-        id: 3892987589
-    },
-
-    {
-        item: 'Use reducers',
-        completed: false,
-        id: 3892987590
-    },
-
-    {
-        item: 'Get reps with reducers',
-        completed: false,
-        id: 3892987591
-    },
-
-]
-
-const Reducer = (state, action) => {
-    switch (action.type) {
-        case "ADD_TODO":
-        // //console.log(state)
-        // return {
-        //     ...state,
-        //     item: action.payload,
-        //     completed: false,
-        //     id: new Date()
-
-        // };
-
-        default:
-            return state;
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987589
     }
-}
+  ]
+      
+    export const todoReducer = (state, action) => {
+      switch (action.type) {
+          case "Add_Todo":
+              return [
+                ...state,
+                  {
+                item: action.payload,
+                completed: false,
+                id: new Date()
+                  }];
+  
+          case "Toggle":
+              let Clickedtoggle =
+              state.map(item => {
+                if (item.id === action.payload) {
+                return {
+                 ...item,
+                 completed: !item.completed
+                  }
+                } else {
+                 return item;
+                 }
+                 });
+                 return Clickedtoggle;
+                       
+          case "Clear_Todo":
+            let Clear =
+            state.filter(item => {
+             if (item.completed === true) {
+                 return !item.completed
+             }else{
+                 return item
+             }
+           }
+         )
+         return Clear;           
+  
+      default:
+              return state;
+      };
+  
+  }
